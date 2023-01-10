8dc72b8b00a06265a06cb4d364bc176f11e289b9
Max Osewoudt <info@maxosewoudt.nl>
2023-01-10 21:15:24 +0100

    Created function

--- project_vertrektijdenbord.ino
+++ project_vertrektijdenbord.ino
@@ -6,6 +6,36 @@
 const char* BASE_URL = "https://api.vertrektijd.info/departures/_nametown/";
 const char* api_key = "X-Vertrektijd-Client-Api-Key";
 
+String full_url = String(BASE_URL) + String(TOWN) + "/" + String(STOP) + "/";  
+
+void api_request() {
+  HTTPClient http;
+  http.begin(full_url);
+  http.addHeader(api_key, API_KEY);
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    String response = http.getString();
+    Serial.println(response);
+    // StaticJsonDocument<2048> doc;
+    // DeserializationError error = deserializeJson(doc, response);
+    // if(!error){
+    //   JsonArray departures = doc["BTMF"][0]["Departures"];
+    //   Serial.println(departures);
+    //   for (JsonObject departure : departures) {
+    //       String lineNumber = departure["LineNumber"];
+    //       String destination = departure["Destination"];
+    //       String expectedDeparture = departure["ExpectedDeparture"];
+    //       Serial.println("Line Number: " + lineNumber + " Destination: " + destination + " Expected Departure: " + expectedDeparture);
+    //   }
+    // } else {
+    //   Serial.println("Error with JSON object");
+    // }
+  } else {
+    Serial.println("Error: " + String(httpCode));
+  }
+  http.end();
+}
+
 void setup() {
   Serial.begin(115200);
   // Connect to Wi-Fi
@@ -15,37 +45,10 @@ void setup() {
     Serial.println("Connecting to WiFi...");
   }
   Serial.println(WiFi.status());
-
-  String full_url = String(BASE_URL) + String(TOWN) + "/" + String(STOP) + "/";
-
-   // Make the API request
-  HTTPClient http;
-  http.begin(full_url);
-  http.addHeader(api_key, API_KEY);
-  int httpCode = http.GET();
-  if (httpCode > 0) {
-    String response = http.getString();
-    StaticJsonDocument<2048> doc;
-    DeserializationError error = deserializeJson(doc, response);
-    if(!error){
-      JsonArray departures = doc["BTMF"][0]["Departures"];
-      Serial.println(departures);
-      for (JsonObject departure : departures) {
-          String lineNumber = departure["LineNumber"];
-          String destination = departure["Destination"];
-          String expectedDeparture = departure["ExpectedDeparture"];
-          Serial.println("Line Number: " + lineNumber + " Destination: " + destination + " Expected Departure: " + expectedDeparture);
-      }
-    } else {
-      Serial.println("Error with JSON object");
-    }
-  } else {
-    Serial.println("Error: " + String(httpCode));
-  }
-  http.end();
-  delay(1000);
 }
 
 void loop() {
-  // Do nothing
+  // Make the API request
+  api_request();
+  delay(10000);
 }
