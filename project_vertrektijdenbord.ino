b060025886ea37a49ac20971e6593931030ed2cd
Max Osewoudt <info@maxosewoudt.nl>
2023-01-10 20:50:13 +0100

    First version is working with an ESP32

--- project_vertrektijdenbord.ino
+++ project_vertrektijdenbord.ino
@@ -1,64 +1,36 @@
-#include <Arduino.h>
-#include <WiFi.h>
 #include <HTTPClient.h>
-#include <ArduinoJson.h>
+#include "credentials.h"
+#include <WiFi.h>
 
-
-const char* ssid = "WiFi Bussum 81";
-const char* password = "#Bussum2023";
+const char* api_url = "https://api.vertrektijd.info/departures/_nametown/Den%20Haag/valkenboslaan/";
+const char* api_key = "X-Vertrektijd-Client-Api-Key";
 
 void setup() {
   Serial.begin(115200);
-  WiFi.begin(ssid, password);
+  // Connect to Wi-Fi
+  WiFi.begin(SSID, PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
-    Serial.println("Connecting to WiFi..");
+    Serial.println("Connecting to WiFi...");
   }
-  Serial.println("Connected to the WiFi network");
-}
+  Serial.println(WiFi.status());
 
-void scrapeTable() {
+   // Make the API request
   HTTPClient http;
-  http.begin("https://www.ns.nl/stationsinformatie/gvc/den-haag-centraal");
+  http.begin(api_url);
+  http.addHeader(api_key, API_KEY);
   int httpCode = http.GET();
-
   if (httpCode > 0) {
-    // HTTP header has been send and Server response header has been handled
-    String payload = http.getString();
-    DynamicJsonDocument doc(1024);
-    deserializeJson(doc, payload);
-
-    // find the table element
-    JsonArray table = doc["ng-star-inserted"];
-    Serial.println(table);
-
-    // iterate over the rows in the table
-    for (JsonObject row : table) {
-      // access the cells in the row
-      String cell1 = row["Tijd"];
-      String cell2 = row["Richting"];
-      Serial.println(cell1 + ", " + cell2);
-      // do something with the cell values
-    }
+    String response = http.getString();
+    Serial.println(response);
+    //Serial.println("Success: " + String(httpCode));
+  } else {
+    Serial.println("Error: " + String(httpCode));
   }
   http.end();
-}
-
-void printWebpage() {
-  HTTPClient http;
-  http.begin("https://www.ns.nl/stationsinformatie/gvc/den-haag-centraal");
-  int httpCode = http.GET();
-  Serial.println(httpCode);
-  if (httpCode > 0) {
-    // HTTP header has been send and Server response header has been handled
-    String payload = http.getString();
-    Serial.println(payload);
-  }
-  http.end();
+  delay(1000);
 }
 
 void loop() {
-  scrapeTable();
-  //printWebpage();
-  delay(5000);
+  // Do nothing
 }
